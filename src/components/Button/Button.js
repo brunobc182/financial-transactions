163b4fda4ccc66832795045105f45bb23af30348
Bruno Braga <bruno.braga@ifactory.com.br>
2018-10-16 14:46:58 -0300

    Create a basic style to button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  bool, func, node, string,
+} from 'prop-types';
+import styled from 'styled-components';
+import colors from '../../theme/colors';
+
+const Button = ({
+  children, onClick, type, isDebit,
+}) => (
+  <ButtonComponent type={type} onClick={onClick} isDebit={isDebit}>
+    {children}
+  </ButtonComponent>
+);
+
+const ButtonComponent = styled.button`
+  width: 100%;
+  max-width: 140px;
+  height: 40px;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.1);
+  background: ${({ isDebit }) => (isDebit ? colors.radicalRed : colors.shamrock)};
+  border-radius: 4px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  color: ${colors.white};
+  font-size: 14px;
+  font-weight: bold;
+  padding: 6px 12px;
+  text-transform: uppercase;
+
+  :hover {
+    background: ${({ isDebit }) => (isDebit ? colors.cinnabar : colors.emerald)};
+  }
+
+  :active {
+    position: relative;
+    top: 1px;
+  }
+`;
+
+Button.propTypes = {
+  children: node,
+  isDebit: bool,
+  onClick: func,
+  type: string,
+};
+
+Button.defaultProps = {
+  children: 'Button text',
+  isDebit: false,
+  onClick: null,
+  type: 'button',
+};
+
+export default Button;
